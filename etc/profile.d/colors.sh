aff7e83253ba78609fd559b4ed1498ecc6a85e2e
Rafael Passos <rafael@rcpassos.me>
2018-11-10 15:37:46 -0200

    color script for proper rofi configuration

---
+++ etc/profile.d/colors.sh
@@ -0,0 +1,4 @@
+export aycolorw="#222222, #222222, #b1b4b3"
+export aycolorn="#222222, #b1b4b3, #222222, #005577, #b1b4b3"
+export aycolora="#222222, #b1b4b3, #222222, #007763, #b1b4b3"
+export aycoloru="#222222, #b1b4b3, #222222, #77003d, #b1b4b3"
