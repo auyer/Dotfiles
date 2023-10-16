46220e3fdbfdd3454dda8ea529f26709f6e478a2
auyer <rafael@rcpassos.me>
2023-10-16 12:15:13 -0300

    adds python configs

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -3,5 +3,15 @@
 -- load configs from files
 reload "user.go"
 reload "user.js"
+reload "user.py"
 reload "user.copilot"
 reload "user.rust"
+
+-- general plugins
+table.insert(lvim.plugins,
+  { "folke/trouble.nvim" }
+)
+
+-- Options
+vim.opt.relativenumber = true
+lvim.transparent_window = true
---
+++ lvim/.config/lvim/lua/user/py.lua
@@ -0,0 +1,91 @@
+lvim.lang.python.lsp.provider = nil
+require("lvim.lsp.manager").setup("ruff_lsp")
+------------------------
+-- Treesitter
+------------------------
+table.insert(lvim.builtin.treesitter.ensure_installed,
+  {
+    "python",
+  })
+
+------------------------
+-- Formatting
+------------------------
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { command = "black", filetypes = { "python" } },
+}
+lvim.format_on_save.enabled = true
+
+table.insert(lvim.format_on_save,
+  {
+    pattern = { "*.py" },
+  }
+)
+
+------------------------
+-- Plugins
+------------------------
+table.insert(lvim.plugins,
+  {
+    "AckslD/swenv.nvim",
+    "stevearc/dressing.nvim",
+    "mfussenegger/nvim-dap-python",
+    "nvim-neotest/neotest",
+    "nvim-neotest/neotest-python",
+    -- 'dccsillag/magma-nvim',
+  }
+)
+
+-- setup linting
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { command = "ruff", filetypes = { "python" } },
+}
+
+-- setup debug adapter
+lvim.builtin.dap.active = true
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+pcall(function()
+  require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+end)
+
+-- setup testing
+require("neotest").setup({
+  adapters = {
+    require("neotest-python")({
+      -- Extra arguments for nvim-dap configuration
+      -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+      dap = {
+        justMyCode = false,
+        console = "integratedTerminal",
+      },
+      args = { "--log-level", "DEBUG", "--quiet" },
+      runner = "pytest",
+    })
+  }
+})
+
+lvim.builtin.which_key.mappings["dm"] = { "<cmd>lua require('neotest').run.run()<cr>",
+  "Test Method" }
+lvim.builtin.which_key.mappings["dM"] = { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>",
+  "Test Method DAP" }
+lvim.builtin.which_key.mappings["df"] = {
+  "<cmd>lua require('neotest').run.run({vim.fn.expand('%')})<cr>", "Test Class" }
+lvim.builtin.which_key.mappings["dF"] = {
+  "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>", "Test Class DAP" }
+lvim.builtin.which_key.mappings["dS"] = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Test Summary" }
+
+
+-- binding for switching
+require("swenv").setup({
+  post_set_env = function()
+    vim.cmd("LspRestart")
+  end,
+})
+
+lvim.builtin.which_key.mappings["v"] = {
+  name = "Python",
+  v = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
+}
+-- lvim.keys.normal_mode["ce"] = "<cmd>lua require('swenv.api').pick_venv()<cr>"
