0c0fc2baac7191696ec499e4b9a1f410d219b493
auyer <rafael@rcpassos.me>
2024-05-02 22:10:38 -0300

    update nvim config with toggleterm

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,10 +3,10 @@
 -- Add any additional keymaps here
 
 -- Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+-- vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
 -- Save with leader W
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
--- nvim/.config/nvim/lua/plugins/general.lua
+++ nvim/.config/nvim/lua/plugins/general.lua
@@ -66,6 +66,7 @@ return {
         "tsx",
         "typescript",
         "yaml",
+        "svelte",
       },
     },
   },
